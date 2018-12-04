b25c1565729cb327408802ab1434587712b69167
bc <bc@LAPTOP-C8TBEKVB>
2018-12-04 21:44:26 +0800

    add test method.add jiyideguiji

--- src/com/bplead/cad/Main.java
+++ src/com/bplead/cad/Main.java
@@ -1,6 +1,11 @@
 package com.bplead.cad;
 
 import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import com.bplead.cad.ui.CADMainFrame;
 import com.bplead.cad.ui.ChooseDrawingDialog;
@@ -19,13 +24,18 @@ public class Main {
 //	    	new LoginFrame().activate();
 //		new CADMainFrame ().activate ();
 //	    new SearchPDMLinkProductDialog(new CADMainFrame ()).activate();
-	    new ChooseDrawingDialog(new CADMainFrame ()).activate();
+//	    new ChooseDrawingDialog(new CADMainFrame ()).activate();
 	    
-//	    new BomDetailDialog(new CADMainFrame ()).activate();
+//	    new BomDetailDialog(null,"TE001",new ArrayList<CADLink>()).activate();
 
 //		new SearchForDownloadDialog(new CADMainFrame ()).activate();
 //	    new FolderChooseDialog(new CADMainFrame (),new SimplePdmLinkProduct()).activate();
 
+	    String ss = Class.class.getClass().getResource("/").getPath();
+	    ss = ss + "dwglist.txt";
+		System.out.println("ss--->"+ss);
+	    String path = getCurrentDrawingPath(ss);
+	    System.out.println("path--->"+path);
 	    }
 	});
     }
@@ -37,4 +47,51 @@ public class Main {
 		{ false, "4", "4x1", "4x2", "4x3", "4x4", "4x5", "4x6" } };
 	return data;
     }
+    
+    public static String getCurrentDrawingPath(String path) {
+    	File file = new File(path);
+    	if(file.exists()) {
+    		try {
+    			FileInputStream fileInputStream = new FileInputStream(file);
+    			String code = getCharset(fileInputStream);
+    			InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream,code);
+    			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+    			StringBuffer sb = new StringBuffer();
+    	    	String text = "";
+    			
+    			while((text = bufferedReader.readLine())!=null) {
+    				sb.append(text);
+    			}
+    			bufferedReader.close();
+    			
+    			return sb.toString();
+    		}catch(Exception e) {
+    			e.printStackTrace();
+    		}
+    	}
+    	return "";
+    }
+    
+	private static String getCharset(FileInputStream fileInputStream) throws IOException {
+    	int p = (fileInputStream.read()<<8)+ fileInputStream.read();
+    	String code = "";
+    	switch(p) {
+    	case 0xefbb:
+    		code = "UTF-8";
+    		break;
+    	case 0xfffe:
+    		code = "Unicode";
+    		break;
+    	case 0xfeff:
+    		code = "UTF-16BE";
+    		break;
+    	default:
+    		code = "GBK";
+
+    	}
+    	return code;
+    	
+    }
 }
+
+
